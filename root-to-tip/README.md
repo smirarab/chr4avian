2c29fa4333cc6c5a7e1ac3d29b3680152748874f
Siavash Mirarab <smirarab@ucsd.edu>
2023-12-14 15:05:06 -0800

    Update root to tip scripts

--- root-to-tip/README.md
+++ root-to-tip/README.md
@@ -1,18 +1,4 @@
-The script [root-to-tip.r](root-to-tip.r) draws the root to tip distance figures in the supplement. 
-
-It uses the following data files:
-
-* mono-mutime2.txt.xz: For each locus that finds Columbea as monophyletic, it gives us (in that order):
-	* The locus id
-	* The branch length above Columbea, 
-	* The number of leaves from Columbea present. 
-* mono-time.txt.xz: For every locus, it includes:
-	*  The locus id
-	*  An extra column with some (ignored) number if the Columbea is monophyletic. This ignored column is our unsuccesful attempt at dating each gene tree, an attempt that was not used in final results. 
-* outliers-rooted.txt
-	* The ID of outlier genes
-
-Before these analyses are performed, all gene trees are rooted. The IDs above refer to the the IDs of rooted gene trees because 8 gene trees that could not be rooted are excluded from analyses (See below for the list). 
+Before these analyses are performed, all gene trees are rooted.
 To root gene trees, we used the following command to root each fully resolved gene tree at the LCA of Palaeognathae.
 
 ~~~ bash
@@ -27,8 +13,24 @@ To root gene trees, we used the following command to root each fully resolved ge
 60281 ERROR: Outgroup's LCA is tree's root - cannot reroot. Try -l.
 61138 ERROR: Outgroup's LCA is tree's root - cannot reroot. Try -l.
 ~~~ 
-
 Here, 
 
 * `63430_named_random_region.gene.trees.filter_mRNA` is the set of gene trees without contracting low support branches. This file is available on an ftp site linked from the main paper repository: https://github.com/smirarab/avian-data/blob/master/ASTRAL/genetrees.txt
 * `annotations.txt` is provided in the file [annotations.txt](annotations.txt)
+
+And:
+
+* These rooted trees are available as `63430_random_region.gene.trees.filter_mRNA_rooted.tre.gz`
+
+### Main figure
+
+The script [root-to-tip.r](root-to-tip.r) draws the root to tip distance figures in the supplement. 
+
+It uses the following data files:
+
+* outliers.txt: Indicates which genes are in the outlier regions. Gene IDs are for all genes not the rooted ones. 
+* mono-mutime2.txt.xz: For each locus that finds Columbea as monophyletic, it gives us the following (in that order). For others, it gives NA. Gene ID is for the rooted ones. 
+	* The locus id
+	* The branch length above Columbea, 
+	* The number of leaves from Columbea present. 
+
--- root-to-tip/root-to-tip.r
+++ root-to-tip/root-to-tip.r
@@ -1,38 +1,37 @@
 require(ggplot2); require(scales); require(reshape2);library(tidyquant);library(zoo)
 
 
-org = read.csv('outliers-rooted.txt',header=F)
+#####################3 Draw root to tip for Columbea
 
-mt = read.csv('mono-time.txt.xz',h=F,sep=" ",col.names = 1:3)
-nrow(mt); head(mt)
+maproot = function(y) {
+  rootfail = c(21952, 32668, 42529, 43658, 45524, 54762, 60281, 61138)
+  vapply(y,function(x) x+sum(x>rootfail),FUN.VALUE = c(1))
+}
 
+mono = read.csv('outliers.txt')
+head(mono)
 mtm = read.csv('mono-mutime2.txt.xz',h=F,sep=" ",col.names = 1:3)
 nrow(mtm); head(mtm)
-mt = merge(mtm,mt,by="X1",all.x = T)
-mt[c(444,42300:42310),]
-names(mt) = c("Gene","mu", "size", "time" ,"size2")
-mt$outlier = mt$Gene %in% org$V1
-mt[c(444,42300:42310),]
-mt[is.na(mt$size) & !is.na(mt$mu),]
-head(mt)
+names(mtm) = c("Gene","mu", "size")
+mtm$Gene = maproot(mtm$Gene)
 
+mtm = merge(mono,mtm,by="Gene")
+table(mtm$outlier)
 
-#summary(mt[!is.na(mt$size) & mt$size>8 & mt$time/mt$mu<1800 & !mt$outlier,"time"])
-#summary(mt[!is.na(mt$size) & mt$size>8 & mt$time/mt$mu<1800 & mt$outlier,"time"])
-summary(mt[!is.na(mt$size) & mt$size>8 & !mt$outlier,"mu"])
-summary(mt[!is.na(mt$size) & mt$size>8 & mt$outlier,"mu"])
+which(mtm$mu>.035)
 
-ggplot(aes(y=mu,x=outlier),data=mt[!is.na(mt$size) & !is.na(mt$mu) & mt$size>8,])+
-    theme_classic()+
-    scale_y_continuous(name="Columbea branch length (sub unit)")+
-    geom_violin(draw_quantiles = c(1/4,1/2,3/4))+
+ggplot(aes(y=mu,x=!outlier),data=mtm)+
+  theme_classic()+
+  scale_y_continuous(name="Columbea branch length (sub unit)")+
+  geom_violin(draw_quantiles = c(1/4,1/2,3/4))+
   stat_summary()+
+  scale_x_discrete(labels=c("Outlier region","Elsewhere"))+
   stat_summary(aes(label=round(..y..,4)),geom="text",position = position_nudge(y = 0.0011))+
   coord_cartesian(ylim=c(0,.033))
 ggsave("Columbea-mrca-subunit.pdf",width=3.4,height =4.2)
 
-#############################  
- 
+######################################
+
 rtt = read.csv('OtidimorphaeColumbea-root-to-tip.txt.xz',sep="\t",header=F)
 annt = read.csv('annotations.txt',sep="\t")
 rtt = merge(rtt,annt,by.x="V1",by.y = "taxa")
@@ -50,4 +49,6 @@ ggplot(aes(x=reorder(V1,V2),y=V2,color=order),data=rtt)+
   theme(axis.text.x = element_text(angle=35,vjust = 1,hjust=1),legend.position = "bottom")
 ggsave("MRCA-to-tip.pdf",width=10,height =6)
 
+
+################################
 
